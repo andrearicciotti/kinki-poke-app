7f386e77673347368c96aec7ab356192ea4e4926
Andrea Ricciotti <139721584+andrearicciotti@users.noreply.github.com>
2025-01-28 23:51:55 +0100

    add: limit checkers and price calculation functions, wip: localStorage for poke cart

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ async function main() {
 
     const data = await response.json();
     config = data[0];
+    
     console.log('File di configurazione: ', config);
         
     generateBoxHTML(data);
@@ -17,12 +18,19 @@ async function main() {
 }
 
 function generateBoxHTML(data) {
-    const poke = data[0];
+    const config = data[0];
     const container = document.querySelector('.ingredients-container');
     let i = 0;
+    let mb = '';
 
-    for (const key in poke.ingredients) {
+    
+    for (const key in config.ingredients) {
         i++;
+
+        if (Object.keys(config.ingredients).length == i) {
+            mb = 'mb-6'
+        }
+
         let translations = {
             bases: "Base",
             proteins: "Proteine",
@@ -32,7 +40,7 @@ function generateBoxHTML(data) {
         }
         
         container.innerHTML += 
-        `<div class="box my-3 text-center">
+        `<div class="box my-2 ${mb} text-center">
             <h2 style="z-index:${i}" class="mt-3 mb-4 py-3 fw-bolder ms-title fs-1">${translations[key]}</h2>
             <ul class="${key+'-list'} d-flex flex-column align-items-center">
             </ul>
@@ -41,15 +49,16 @@ function generateBoxHTML(data) {
 }
 
 function generateIngredientsHTML(data) {
-    const poke = data[0];
+    const config = data[0];
     let list;
 
-    for (const key in poke.ingredients) {
+    for (const key in config.ingredients) {
         
-        poke.ingredients[key].forEach(ingredient => {
+        config.ingredients[key].forEach(ingredient => {
             list = document.querySelector('.'+key+'-list');
+
             list.innerHTML += 
-            `<li class="ingredient my-2 d-flex align-items-center justify-content-between position-relative"><i class="fa-solid fa-minus me-3" onclick="btnClick(false,'${key}','${ingredient}')"></i></i><span class="ingredient-text">${ingredient}</span><i class="fa-solid fa-plus ms-3" onclick="btnClick(true,'${key}','${ingredient}')"></i><span id="${key}-${ingredient}" class="ingredient-badge position-absolute ingredient-badge-hidden"></span></li>`;
+            `<li class="ingredient my-2 d-flex align-items-center justify-content-between position-relative"><i class="fa-solid fa-minus me-3" onclick="btnClick(false,'${key}','${ingredient.replace("'", "\\'")}')"></i></i><span class="ingredient-text">${ingredient}</span><i class="fa-solid fa-plus ms-3" onclick="btnClick(true,'${key}','${ingredient.replace("'", "\\'")}')"></i><span id="${key}-${ingredient}" class="ingredient-badge position-absolute badge-hidden"></span></li>`;
         });
     }
 }
@@ -58,16 +67,27 @@ main();
 
 function btnClick(add, type, ingredient) {
     let badge = document.getElementById(type+'-'+ingredient);
-    console.log(add, type, ingredient);
+    let priceContainer = document.querySelector('.price');
 
     if (add) {
+        if (priceContainer.innerText == '') {
+            priceContainer.innerText = `${config.price} €`;    
+        }
+
         if (!poke[type]) {
             poke[type] = {}
         }
 
-        poke[type][ingredient] = (poke[type][ingredient] | 0) + 1;
-        badge.innerText = poke[type][ingredient];
-        badge.classList.remove('ingredient-badge-hidden');
+        if (checkMaximals(type)) {
+            if (poke[type][ingredient]) {
+                poke[type][ingredient] ++;
+            } else {
+                poke[type][ingredient] = 1;
+            }
+    
+            badge.innerText = poke[type][ingredient];
+            badge.classList.remove('badge-hidden');
+        }
     }
 
     if (!add) {
@@ -77,12 +97,57 @@ function btnClick(add, type, ingredient) {
         if (poke[type][ingredient] > 0) {
             badge.innerText = poke[type][ingredient];
         } else {
-            badge.classList.add('ingredient-badge-hidden');
+            badge.classList.add('badge-hidden');
         }
     }
 
-    console.log(poke[type]);
+    checkLimits();
+}
+
+function checkMaximals(type) {
+    let maximals = config.maximals[type]
+    let quantity;
     
+    for (const keyType in poke) {
+        quantity = 1;
+        for (const keyIngredient in poke[keyType]) {
+            quantity += poke[keyType][keyIngredient];
+        }
+    }
+
+    return quantity <= maximals
+}
+
+function checkLimits() {
+    let priceContainer = document.querySelector('.price');
+    let price = parseFloat(config.price);
+    let toAdd = 0;
+
+    for (const keyType in poke) {
+        let limit = config.limits[keyType];
+        let quantity = 0;
+
+        for (const keyIngredient in poke[keyType]) {
+            quantity += poke[keyType][keyIngredient];
+        }
+
+        if (quantity > 0) {
+            priceContainer.innerText = `${price.toFixed(2)} €`;
+        } else {
+            priceContainer.innerText = '';
+        }
+
+        if (quantity > limit) {
+            toAdd += (quantity - limit) * config.extra[keyType];
+        }
+    }
+
+    if (toAdd > 0) {
+        price += toAdd;
+    }
+
+    priceContainer.innerText = `${price.toFixed(2)} €`;
+    poke['price'] = price;
 }
 
 function checkStickyStatus() {
@@ -100,6 +165,20 @@ function checkStickyStatus() {
             title.style.backgroundColor = '';
         }
     });
+}
+
+function savePoke() {
+    let storage = JSON.parse(localStorage.getItem('cart')) || [];
+    console.log(storage);
+
+    storage.cart.push(poke);
+    localStorage.setItem('cart', JSON.stringify(storage.cart));
+
+    const totalPrice = storage.cart.reduce((total, currentPoke) => total + currentPoke.price, 0);
+
+    localStorage.setItem('cart', JSON.stringify({ cart: storage.cart, totalPrice: totalPrice }));
+
+    poke = {};
 }
 
 window.addEventListener('scroll', checkStickyStatus);
