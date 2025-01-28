65db7c5093e5f1cd226c7ecc0b11e5ac2b93674e
andrearicciotti <ricciotti.andrea1993@gmail.cim>
2025-01-28 14:04:24 +0100

    add: buttons to configure poke, structure for poke and badge for enumerate poke ingredients

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+let config;
+let poke = {};
+
 async function main() {
     const response = await fetch('../config.json');
 
@@ -6,6 +9,8 @@ async function main() {
     }
 
     const data = await response.json();
+    config = data[0];
+    console.log('File di configurazione: ', config);
         
     generateBoxHTML(data);
     generateIngredientsHTML(data);
@@ -14,15 +19,13 @@ async function main() {
 function generateBoxHTML(data) {
     const poke = data[0];
     const container = document.querySelector('.ingredients-container');
-    console.log(data);
 
     for (const key in poke.ingredients) {
-        console.log(key);
         
         container.innerHTML += 
         `<div class="box my-3 text-center">
-            <h2>${key}</h2>
-            <ul class="${key+'-list'}">
+            <h2 class="mt-3 mb-3 fw-bold">${key}</h2>
+            <ul class="${key+'-list'} d-flex flex-column align-items-center">
             </ul>
         </div>`;
     }
@@ -37,9 +40,38 @@ function generateIngredientsHTML(data) {
         poke.ingredients[key].forEach(ingredient => {
             list = document.querySelector('.'+key+'-list');
             list.innerHTML += 
-            `<li><span class"btn-reduce>- </span>${ingredient}<span class="btn-increase"> +</span></li>`;
+            `<li class="ingredient my-2 d-flex align-items-center position-relative"><i class="fa-solid fa-minus me-3" onclick="btnClick(false,'${key}','${ingredient}')"></i></i><span class="ingredient-text">${ingredient}</span><i class="fa-solid fa-plus ms-3" onclick="btnClick(true,'${key}','${ingredient}')"></i><span id="${key}-${ingredient}" class="ingredient-badge position-absolute ingredient-badge-hidden"></span></li>`;
         });
     }
 }
 
 main();
+
+function btnClick(add, type, ingredient) {
+    let badge = document.getElementById(type+'-'+ingredient);
+    console.log(add, type, ingredient);
+
+    if (add) {
+        if (!poke[type]) {
+            poke[type] = {}
+        }
+
+        poke[type][ingredient] = (poke[type][ingredient] | 0) + 1;
+        badge.innerText = poke[type][ingredient];
+        badge.classList.remove('ingredient-badge-hidden');
+    }
+
+    if (!add) {
+        if (poke[type][ingredient] > 0) {
+            poke[type][ingredient] -= 1;
+        }
+        if (poke[type][ingredient] > 0) {
+            badge.innerText = poke[type][ingredient];
+        } else {
+            badge.classList.add('ingredient-badge-hidden');
+        }
+    }
+
+    console.log(poke[type]);
+    
+}
